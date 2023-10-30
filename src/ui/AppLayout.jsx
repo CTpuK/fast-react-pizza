20eeb2b4727733028c1b253f8d075f5d287d1146
Evgeny Strikunov <strik.jenya@gmail.com>
2023-10-30 10:11:53 +0300

    9 commit

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -5,6 +5,7 @@ import Loader from "./Loader";
 
 function AppLayout() {
   const navigation = useNavigation();
+  // eslint-disable-next-line no-unused-vars
   const isLoading = navigation.state === "loading";
 
   return (
